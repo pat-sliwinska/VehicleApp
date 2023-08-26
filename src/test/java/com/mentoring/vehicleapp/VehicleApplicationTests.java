617ab64cf726f0db8e5b093ab9db10cdb8947051
Patrycja Śliwińska <psliwinska@objectivity.co.uk>
2023-08-26 11:57:41 +0200

    initial commit with H2 database configuration + Flyway

---
+++ src/test/java/com/mentoring/vehicleapp/VehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoring.vehicleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
